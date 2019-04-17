00844bf48e503f7c48e608cd17edf417742b4bff
zlayabekrija <zlaja.bekrija@gmail.com>
2019-04-17 11:42:56 +0200

    'firstTests'

---
+++ calculator.js
@@ -0,0 +1,9 @@
+
+const calculator = (()=>{
+    const sum = (a,b) => (a + b);
+    const subtract = (a,b) => (a - b);
+    const multiply = (a,b) => (a * b);
+    const divide = (a,b) => (a/b);
+    return {sum,subtract,multiply,divide}
+})
+export default calculator;
